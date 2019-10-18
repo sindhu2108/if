16dca836dab93f440785f2fd359ac5ce6dec710e
sindhu2108 <sindhuvemulamandha@gmail.com>
2019-10-17 21:00:22 -0400

    first commit

--- if.sh
+++ if.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 int1=$1
 int2=$2
-if [ $int1 -gt $int2 ]; then
+if [ $int1 -eq $int2 ]; then
+echo "values are equal"
+elif [ $int1 -gt  $int2 ]; then
 echo "values are grater"
-else
+else 
 echo "values are not grater"
 fi
